276a02e4ffb18dc6fa9508198c2e86dfc75f95e8
Natte2110 <natteparsley@gmail.com>
2024-02-20 15:53:00 +0000

    feat: Add functions to show and hide password

--- tracktician/static/js/log-in.js
+++ tracktician/static/js/log-in.js
@@ -4,6 +4,45 @@
  * @copyright Nathan Parsley 2024
  */
 $(document).ready(function () {
+
+    const passwordVisibility = () => {
+        if ($("#password").attr("type") === "password") {
+            $("#password").attr("type", "text");
+            $("#show-password").addClass("d-none");
+            $("#hide-password").removeClass("d-none");
+        } else {
+            $("#password").attr("type", "password");
+            $("#hide-password").addClass("d-none");
+            $("#show-password").removeClass("d-none");
+        }
+    }
+
+    $("#show-password").click(function () {
+        passwordVisibility();
+    });
+    $("#hide-password").click(function () {
+        passwordVisibility();
+    });
+
+    const confirmPasswordVisibility = () => {
+        if ($("#confirm-password").attr("type") === "password") {
+            $("#confirm-password").attr("type", "text");
+            $("#show-confirm-password").addClass("d-none");
+            $("#hide-confirm-password").removeClass("d-none");
+        } else {
+            $("#confirm-password").attr("type", "password");
+            $("#hide-confirm-password").addClass("d-none");
+            $("#show-confirm-password").removeClass("d-none");
+        }
+    }
+    
+    $("#show-confirm-password").click(function () {
+        confirmPasswordVisibility();
+    });
+    $("#hide-confirm-password").click(function () {
+        confirmPasswordVisibility();
+    });
+    
     $('#login-form').submit(function (event) {
         event.preventDefault();
 
